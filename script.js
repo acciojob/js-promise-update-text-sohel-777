6125e69d82ac89c70bf62cb54859ac5c6943e073
SK SOHEL RAHAMAN <sohelrahaman43@gmail.com>
2023-03-18 15:57:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 //your JS code here. If required.
 function myPromise (){
-	return new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Hello, world!");
-    
-  }, 1000);
+	return new Promise((resolve) => {
+	  setTimeout(() => { 
+		  resolve("Hello, world!");
+	  }, 1000);
+    });
 }
+myPromise.then((value) => document.querySelector(''#output').innerText= value);
 
-
-myPromise
-  .then((res) => {
-   document.querySelector(#output).textContent=res;
-   
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+// function func(){
+// 	return new Promise((resolve) => {
+// 		setTimeout(() => {
+// 			resolve("Hello, world!");
+// 		}, 1000);
+// 	});
+// }
+// func().then((value) => document.getElementById("output").innerText = value);
+  
